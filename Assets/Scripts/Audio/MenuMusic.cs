ad28f0ade46e1c52c1d071ff13671f430dcaac0e
bajtoszek <krzybar@staszic.waw.pl>
2022-01-23 16:27:26 +0100

    Music

---
+++ Assets/Scripts/Audio/MenuMusic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().PlayBackgroundMusic(0);
+    }
+}
